80d065eda5728640762b7a5997a711a4e592b4f4
Abduino <abdrehmanredi0@gmail.com>
2022-09-20 15:34:21 -0300

    object file

---
+++ Text Reader/tReader.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class tReader
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(tReader))
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.lblPath = New System.Windows.Forms.Label()
+        Me.txtGet = New System.Windows.Forms.TextBox()
+        Me.txtRead = New System.Windows.Forms.TextBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.btnRead = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnClear
+        '
+        resources.ApplyResources(Me.btnClear, "btnClear")
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'lblPath
+        '
+        resources.ApplyResources(Me.lblPath, "lblPath")
+        Me.lblPath.Name = "lblPath"
+        '
+        'txtGet
+        '
+        resources.ApplyResources(Me.txtGet, "txtGet")
+        Me.txtGet.Name = "txtGet"
+        '
+        'txtRead
+        '
+        Me.txtRead.AcceptsReturn = True
+        Me.txtRead.BackColor = System.Drawing.SystemColors.Menu
+        resources.ApplyResources(Me.txtRead, "txtRead")
+        Me.txtRead.Name = "txtRead"
+        '
+        'ProgressBar1
+        '
+        resources.ApplyResources(Me.ProgressBar1, "ProgressBar1")
+        Me.ProgressBar1.Name = "ProgressBar1"
+        '
+        'btnRead
+        '
+        resources.ApplyResources(Me.btnRead, "btnRead")
+        Me.btnRead.Name = "btnRead"
+        Me.btnRead.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        resources.ApplyResources(Me.TextBox3, "TextBox3")
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ReadOnly = True
+        '
+        'btnBrowse
+        '
+        resources.ApplyResources(Me.btnBrowse, "btnBrowse")
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button1
+        '
+        resources.ApplyResources(Me.Button1, "Button1")
+        Me.Button1.Name = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'tReader
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.btnRead)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.txtRead)
+        Me.Controls.Add(Me.txtGet)
+        Me.Controls.Add(Me.lblPath)
+        Me.Controls.Add(Me.btnClear)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "tReader"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents lblPath As System.Windows.Forms.Label
+    Friend WithEvents txtGet As System.Windows.Forms.TextBox
+    Friend WithEvents txtRead As System.Windows.Forms.TextBox
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents btnRead As System.Windows.Forms.Button
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents btnBrowse As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ Text Reader/tReader.vb
@@ -0,0 +1,80 @@
+﻿Public Class tReader
+
+   
+    Private Sub btnRead_Click(sender As Object, e As EventArgs) Handles btnRead.Click
+        Dim SAPI
+        SAPI = CreateObject("SAPI.SPVOICE")
+        SAPI.SPEAK(txtRead.Text)
+    End Sub
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtRead.Clear()
+        txtGet.Clear()
+    End Sub
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        ProgressBar1.Value = 0
+        OpenFileDialog1.ShowDialog()
+
+        txtGet.Text = OpenFileDialog1.FileName
+        If System.IO.File.Exists(txtGet.Text) = True Then
+            For X As Integer = 1 To 100
+
+                ProgressBar1.Value = ProgressBar1.Value + 1
+            Next
+            Dim objreader As New System.IO.StreamReader(txtGet.Text)
+            txtRead.Text = objreader.ReadToEnd
+            objreader.Close()
+        End If
+
+    End Sub
+
+    Private Sub btnRead_MouseHover(sender As Object, e As EventArgs) Handles btnRead.MouseHover
+        Dim SAPI
+        SAPI = CreateObject("SAPI.SPVOICE")
+        SAPI.SPEAK(btnRead.Text)
+
+    End Sub
+
+    Private Sub btnBrowse_MouseHover(sender As Object, e As EventArgs) Handles btnBrowse.MouseHover
+        Dim SAPI
+        SAPI = CreateObject("SAPI.SPVOICE")
+        SAPI.SPEAK(btnBrowse.Text)
+    End Sub
+
+ 
+
+    Private Sub btnClear_MouseHover(sender As Object, e As EventArgs) Handles btnClear.MouseHover
+        Dim SAPI
+        SAPI = CreateObject("SAPI.SPVOICE")
+        SAPI.SPEAK(btnClear.Text)
+    End Sub
+
+
+
+   
+    Private Sub tReader_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Dim SAPI
+        SAPI = CreateObject("SAPI.SPVOICE")
+        SAPI.SPEAK("the program is shuting down")
+    End Sub
+
+
+
+
+    Private Sub tReader_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim SAPI
+        SAPI = CreateObject("SAPI.SPVOICE")
+        SAPI.SPEAK("well come to T reader")
+    End Sub
+
+    Private Sub Button1_Click_2(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+
+    End Sub
+
+    Private Sub Button1_MouseHover(sender As Object, e As EventArgs) Handles Button1.MouseHover
+        Dim SAPI
+        SAPI = CreateObject("SAPI.SPVOICE")
+        SAPI.SPEAK("close the program")
+    End Sub
+End Class
