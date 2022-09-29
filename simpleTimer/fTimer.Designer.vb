c58ffcc7cef43cf87e064c44a3b8a9a69b0cb46c
Abduino <abdrehmanredi0@gmail.com>
2022-09-29 15:14:16 -0300

    simple timer project added

---
+++ simpleTimer/fTimer.Designer.vb
@@ -0,0 +1,179 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class fTimer
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnSpeak = New System.Windows.Forms.Button()
+        Me.txt1 = New System.Windows.Forms.TextBox()
+        Me.txt2 = New System.Windows.Forms.TextBox()
+        Me.txt3 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.btnFinish = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txt0 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnSpeak
+        '
+        Me.btnSpeak.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnSpeak.Location = New System.Drawing.Point(12, 73)
+        Me.btnSpeak.Name = "btnSpeak"
+        Me.btnSpeak.Size = New System.Drawing.Size(130, 23)
+        Me.btnSpeak.TabIndex = 0
+        Me.btnSpeak.Text = "SPEAK"
+        Me.btnSpeak.UseVisualStyleBackColor = True
+        '
+        'txt1
+        '
+        Me.txt1.Location = New System.Drawing.Point(47, 47)
+        Me.txt1.Name = "txt1"
+        Me.txt1.Size = New System.Drawing.Size(27, 20)
+        Me.txt1.TabIndex = 3
+        Me.txt1.Text = "00"
+        '
+        'txt2
+        '
+        Me.txt2.Location = New System.Drawing.Point(81, 47)
+        Me.txt2.Name = "txt2"
+        Me.txt2.Size = New System.Drawing.Size(27, 20)
+        Me.txt2.TabIndex = 4
+        Me.txt2.Text = "00"
+        '
+        'txt3
+        '
+        Me.txt3.Location = New System.Drawing.Point(114, 47)
+        Me.txt3.Name = "txt3"
+        Me.txt3.Size = New System.Drawing.Size(25, 20)
+        Me.txt3.TabIndex = 5
+        Me.txt3.Text = "00"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 60
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(12, 12)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(62, 23)
+        Me.btnStart.TabIndex = 9
+        Me.btnStart.Text = "START"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'btnFinish
+        '
+        Me.btnFinish.Location = New System.Drawing.Point(80, 12)
+        Me.btnFinish.Name = "btnFinish"
+        Me.btnFinish.Size = New System.Drawing.Size(62, 23)
+        Me.btnFinish.TabIndex = 10
+        Me.btnFinish.Text = "FINISH"
+        Me.btnFinish.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 143)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(130, 82)
+        Me.ListBox1.TabIndex = 11
+        '
+        'btnAdd
+        '
+        Me.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnAdd.Location = New System.Drawing.Point(12, 114)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(130, 23)
+        Me.btnAdd.TabIndex = 12
+        Me.btnAdd.Text = "ADD"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnClear.Location = New System.Drawing.Point(12, 94)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(130, 23)
+        Me.btnClear.TabIndex = 13
+        Me.btnClear.Text = "CLEAR"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 231)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(130, 20)
+        Me.TextBox1.TabIndex = 14
+        Me.TextBox1.Text = "+251912381703(ABD)"
+        '
+        'txt0
+        '
+        Me.txt0.Location = New System.Drawing.Point(12, 47)
+        Me.txt0.Name = "txt0"
+        Me.txt0.Size = New System.Drawing.Size(26, 20)
+        Me.txt0.TabIndex = 15
+        Me.txt0.Text = "00"
+        '
+        'fTimer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.ClientSize = New System.Drawing.Size(151, 254)
+        Me.Controls.Add(Me.txt0)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.btnFinish)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.txt3)
+        Me.Controls.Add(Me.txt2)
+        Me.Controls.Add(Me.txt1)
+        Me.Controls.Add(Me.btnSpeak)
+        Me.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "fTimer"
+        Me.Text = "fTimer"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnSpeak As System.Windows.Forms.Button
+    Friend WithEvents txt1 As System.Windows.Forms.TextBox
+    Friend WithEvents txt2 As System.Windows.Forms.TextBox
+    Friend WithEvents txt3 As System.Windows.Forms.TextBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents btnStart As System.Windows.Forms.Button
+    Friend WithEvents btnFinish As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents txt0 As System.Windows.Forms.TextBox
+
+End Class
---
+++ simpleTimer/fTimer.vb
@@ -0,0 +1,65 @@
+﻿Public Class fTimer
+    Dim x As Integer
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If txt3.Text <> 60 Then
+            txt3.Text = txt3.Text + 1
+        Else
+            txt3.Text = 0
+            If txt2.Text <> 60 Then
+                txt2.Text = txt2.Text + 1
+            Else
+                txt2.Text = 0
+                If txt1.Text <> 60 Then
+                    txt1.Text = txt1.Text + 1
+                Else
+                    txt1.Text = 0
+                    If txt0.Text <> 60 Then
+                        txt0.Text = txt0.Text + 1
+                    Else
+                        txt0.Text = 0
+                    End If
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        Timer1.Start()
+        Timer1.Interval = 20
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnFinish.Click
+        Timer1.Stop()
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnSpeak.Click
+        Dim SAPI
+        SAPI = CreateObject("SAPI.SPVOICE")
+        SAPI.SPEAK("THE TIME IS    " & txt0.Text & "time and" & txt1.Text & " minute and" & txt2.Text & "second and" & txt3.Text & "micro second")
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        X = X + 1
+        ListBox1.Items.Add(x & "," & txt0.Text & ":" & txt1.Text & ":" & txt2.Text & ":" & txt3.Text)
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txt0.Text = "00"
+        txt1.Text = "00"
+        txt2.Text = "00"
+        txt3.Text = "00"
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
