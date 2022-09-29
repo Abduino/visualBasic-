4169a62584bedd3f867b51bcd12236320f57fb35
Abduino <abdrehmanredi0@gmail.com>
2022-09-29 15:26:42 -0300

    login functionality added

---
+++ Employee/project1/account.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLogin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtUserName = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.lblload = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(18, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(98, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "User Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(18, 78)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(86, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Password"
+        '
+        'txtUserName
+        '
+        Me.txtUserName.Location = New System.Drawing.Point(132, 25)
+        Me.txtUserName.Name = "txtUserName"
+        Me.txtUserName.Size = New System.Drawing.Size(230, 20)
+        Me.txtUserName.TabIndex = 2
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(132, 78)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPassword.Size = New System.Drawing.Size(230, 20)
+        Me.txtPassword.TabIndex = 3
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLogin.Location = New System.Drawing.Point(377, 22)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(75, 23)
+        Me.btnLogin.TabIndex = 4
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(377, 75)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(132, 121)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(320, 20)
+        Me.ProgressBar1.TabIndex = 7
+        '
+        'lblload
+        '
+        Me.lblload.AutoSize = True
+        Me.lblload.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblload.Location = New System.Drawing.Point(18, 121)
+        Me.lblload.Name = "lblload"
+        Me.lblload.Size = New System.Drawing.Size(93, 20)
+        Me.lblload.TabIndex = 8
+        Me.lblload.Text = "Loading...."
+        '
+        'Timer1
+        '
+        '
+        'frmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.SkyBlue
+        Me.ClientSize = New System.Drawing.Size(465, 144)
+        Me.Controls.Add(Me.lblload)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUserName)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "frmLogin"
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtUserName As System.Windows.Forms.TextBox
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents btnLogin As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents lblload As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ Employee/project1/account.vb
@@ -0,0 +1,81 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Data.OleDb
+Module connect
+    Public conn As New SqlConnection("Data Source=ABDREHMAN-PC;Initial Catalog=hu;Integrated Security=True")
+    ' making account table connection publuic 
+    Public dslogin As New DataSet
+    Public strSelectaccount As String = "Select * FROM account"
+    Public dalogin As New SqlDataAdapter(strSelectaccount, conn)
+    Public dtlogin As New DataTable
+    ' making employeee table connection publuic 
+    Public dsemp As New DataSet
+    Public strSelectemployee As String = "Select * FROM employee"
+    Public daemp As New SqlDataAdapter(strSelectemployee, conn)
+    Public dtemp As New DataTable
+    Public row As Integer
+End Module
+Public Class frmLogin
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ProgressBar1.Hide()
+        lblload.Hide()
+    End Sub
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        Try
+            If txtUserName.Text <> "" Then
+                If txtPassword.Text <> "" Then
+                    strSelectaccount = "select * from account where username='" & txtUserName.Text & "' and password='" & txtPassword.Text & "'"
+                    Dim dalogin As New SqlDataAdapter(strSelectaccount, conn)
+                    conn.Open()
+                    dalogin.Fill(dslogin, "accounttable")
+                    conn.Close()
+                    dtlogin = dslogin.Tables("accounttable")
+                    row = dtlogin.Rows.Count
+                    If row > 0 Then
+                        Timer1.Start()
+                        lblload.Show()
+                        ProgressBar1.Show()
+                    Else
+                        MessageBox.Show("User name or Password is Incorrect!", "Infoming", MessageBoxButtons.OKCancel, MessageBoxIcon.Information)
+                        If DialogResult.OK Then
+                            txtUserName.Text = ""
+                            txtPassword.Text = ""
+                            txtUserName.Focus()
+                        End If
+                    End If
+                Else
+                    MessageBox.Show("Password is required", "Informing", MessageBoxButtons.OKCancel, MessageBoxIcon.Information)
+                    If DialogResult.OK Then
+                        txtPassword.Focus()
+                    End If
+                End If
+            Else
+                MessageBox.Show("User Name is required", "Informing", MessageBoxButtons.OKCancel, MessageBoxIcon.Information)
+                If DialogResult.OK Then
+                    txtUserName.Focus()
+                End If
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+    Private Sub btnClear_Click(sender As Object, e As EventArgs)
+        txtUserName.Text = ""
+        txtPassword.Text = ""
+        txtUserName.Focus()
+    End Sub
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ProgressBar1.Increment(1)
+        If ProgressBar1.Value = ProgressBar1.Maximum Then
+            Timer1.Stop()
+            txtUserName.Clear()
+            txtPassword.Clear()
+            ProgressBar1.Hide()
+            lblload.Hide()
+            employee.Show()
+        End If
+    End Sub
+End Class
